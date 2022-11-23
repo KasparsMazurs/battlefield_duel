7bfc4e766bcebd90bf3f111876e12215ee05a2c4
KasparsMazurs <kaspars.mazurs@gmail.com>
2022-11-23 17:52:19 +0000

    change visibility options for explosions images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
- 
+// This function will allow choosing a weapon. If the player carries a pistol he will be allowed to shout at two places simultaneously
 function confirm_gun() {
     let weapon = document.querySelector('input[name="weapon"]:checked').value
     if (weapon === "pistol"){
