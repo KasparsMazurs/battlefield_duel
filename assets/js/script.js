065fd7b6a2d3642b0081918f8ec46ee3ebc8123e
KasparsMazurs <kaspars.mazurs@gmail.com>
2022-11-23 17:41:31 +0000

    create function that will allow choosing a weapon

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+ 
+function confirm_gun() {
+    let weapon = document.querySelector('input[name="weapon"]:checked').value
+    if (weapon === "pistol"){
+       document.getElementById("second_gun").style.visibility = "visible";
+    } else {
+        document.getElementById("second_gun").style.visibility = "hidden";
+    }
+}
